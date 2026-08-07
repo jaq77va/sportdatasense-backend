5af40938ed138150ad73a3eea56afc86dedb79b6
jaq77va <jaq77va@gmail.com>
2026-08-07 21:26:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -90,8 +90,8 @@ def chat_gpx():
     try:
         # Chiamata a Gemini 2.0 Flash
         response = client.models.generate_content(
-        # AQ -model='gemini-2.0-flash',
-        model='gemini-1.5-flash',
+        model='gemini-2.0-flash',
+       # AQ - model='gemini-1.5-flash',
             contents=context,
         )
         answer = response.text
